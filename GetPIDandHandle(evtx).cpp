9dd7347bd38d573f920a658d6bc7e985392ec278
3gstudent <robertpetersen@outlook.com>
2018-07-19 05:15:27 -0700

    Create GetPIDandHandle(evtx).cpp

---
+++ GetPIDandHandle(evtx).cpp
@@ -0,0 +1,283 @@
+#include <windows.h>
+#include <stdio.h>
+#pragma comment(lib,"Advapi32.lib") 
+
+#define NT_SUCCESS(x) ((x) >= 0)
+#define STATUS_INFO_LENGTH_MISMATCH 0xc0000004
+#define SystemHandleInformation 16
+#define ObjectBasicInformation 0
+#define ObjectNameInformation 1
+#define ObjectTypeInformation 2
+
+typedef NTSTATUS(NTAPI *_NtQuerySystemInformation)(
+	ULONG SystemInformationClass,
+	PVOID SystemInformation,
+	ULONG SystemInformationLength,
+	PULONG ReturnLength
+	);
+typedef NTSTATUS(NTAPI *_NtDuplicateObject)(
+	HANDLE SourceProcessHandle,
+	HANDLE SourceHandle,
+	HANDLE TargetProcessHandle,
+	PHANDLE TargetHandle,
+	ACCESS_MASK DesiredAccess,
+	ULONG Attributes,
+	ULONG Options
+	);
+typedef NTSTATUS(NTAPI *_NtQueryObject)(
+	HANDLE ObjectHandle,
+	ULONG ObjectInformationClass,
+	PVOID ObjectInformation,
+	ULONG ObjectInformationLength,
+	PULONG ReturnLength
+	);
+
+typedef struct _UNICODE_STRING
+{
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _SYSTEM_HANDLE
+{
+	ULONG ProcessId;
+	BYTE ObjectTypeNumber;
+	BYTE Flags;
+	USHORT Handle;
+	PVOID Object;
+	ACCESS_MASK GrantedAccess;
+} SYSTEM_HANDLE, *PSYSTEM_HANDLE;
+
+typedef struct _SYSTEM_HANDLE_INFORMATION
+{
+	ULONG HandleCount;
+	SYSTEM_HANDLE Handles[1];
+} SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;
+
+typedef enum _POOL_TYPE
+{
+	NonPagedPool,
+	PagedPool,
+	NonPagedPoolMustSucceed,
+	DontUseThisType,
+	NonPagedPoolCacheAligned,
+	PagedPoolCacheAligned,
+	NonPagedPoolCacheAlignedMustS
+} POOL_TYPE, *PPOOL_TYPE;
+
+typedef struct _OBJECT_TYPE_INFORMATION
+{
+	UNICODE_STRING Name;
+	ULONG TotalNumberOfObjects;
+	ULONG TotalNumberOfHandles;
+	ULONG TotalPagedPoolUsage;
+	ULONG TotalNonPagedPoolUsage;
+	ULONG TotalNamePoolUsage;
+	ULONG TotalHandleTableUsage;
+	ULONG HighWaterNumberOfObjects;
+	ULONG HighWaterNumberOfHandles;
+	ULONG HighWaterPagedPoolUsage;
+	ULONG HighWaterNonPagedPoolUsage;
+	ULONG HighWaterNamePoolUsage;
+	ULONG HighWaterHandleTableUsage;
+	ULONG InvalidAttributes;
+	GENERIC_MAPPING GenericMapping;
+	ULONG ValidAccess;
+	BOOLEAN SecurityRequired;
+	BOOLEAN MaintainHandleCount;
+	USHORT MaintainTypeList;
+	POOL_TYPE PoolType;
+	ULONG PagedPoolUsage;
+	ULONG NonPagedPoolUsage;
+} OBJECT_TYPE_INFORMATION, *POBJECT_TYPE_INFORMATION;
+
+PVOID GetLibraryProcAddress(PSTR LibraryName, PSTR ProcName)
+{
+	return GetProcAddress(GetModuleHandleA(LibraryName), ProcName);
+}
+
+BOOL EnableDebugPrivilege(BOOL fEnable)
+{
+	BOOL fOk = FALSE;
+	HANDLE hToken;
+
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+	{
+		TOKEN_PRIVILEGES tp;
+		tp.PrivilegeCount = 1;
+		LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &tp.Privileges[0].Luid);
+		tp.Privileges[0].Attributes = fEnable ? SE_PRIVILEGE_ENABLED : 0;
+		AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), NULL, NULL);
+		fOk = (GetLastError() == ERROR_SUCCESS);
+		CloseHandle(hToken);
+	}
+	return(fOk);
+}
+
+DWORD  getpid()
+{
+	DWORD PID = 0;
+	SC_HANDLE scHandle = OpenSCManager(NULL, NULL, SC_MANAGER_ENUMERATE_SERVICE);
+	if (scHandle == NULL)
+	{
+		printf("[!]OpenSCManager fail(%ld)", GetLastError());
+	}
+	else
+	{
+		SC_ENUM_TYPE infoLevel = SC_ENUM_PROCESS_INFO;
+		DWORD dwServiceType = SERVICE_WIN32;
+		DWORD dwServiceState = SERVICE_STATE_ALL;
+		LPBYTE lpServices = NULL;
+		DWORD cbBufSize = 0;
+		DWORD pcbBytesNeeded;
+		DWORD servicesReturned;
+		LPDWORD lpResumeHandle = NULL;
+		LPCSTR pszGroupName = NULL;
+		BOOL ret = EnumServicesStatusEx(scHandle, infoLevel, dwServiceType, dwServiceState, lpServices, cbBufSize, &pcbBytesNeeded, &servicesReturned, lpResumeHandle, pszGroupName);
+		cbBufSize = pcbBytesNeeded;
+		lpServices = new BYTE[cbBufSize];
+		if (NULL == lpServices)
+		{
+			printf("[!]lpServices = new BYTE[%ld] -> fail(%ld)\n", cbBufSize, GetLastError());
+		}
+		else
+		{
+			ret = EnumServicesStatusEx(scHandle, infoLevel, dwServiceType, dwServiceState, lpServices, cbBufSize, &pcbBytesNeeded, &servicesReturned, lpResumeHandle, pszGroupName);
+			LPENUM_SERVICE_STATUS_PROCESS lpServiceStatusProcess = (LPENUM_SERVICE_STATUS_PROCESS)lpServices;
+			for (DWORD i = 0; i < servicesReturned; i++)
+			{
+				_strlwr_s(lpServiceStatusProcess[i].lpServiceName, strlen(lpServiceStatusProcess[i].lpServiceName) + 1);
+				if (strstr(lpServiceStatusProcess[i].lpServiceName, "eventlog") != 0)
+				{
+					printf("[+]ServiceName:%s\n", lpServiceStatusProcess[i].lpServiceName);
+					printf("[+]PID:%ld\n", lpServiceStatusProcess[i].ServiceStatusProcess.dwProcessId);
+					PID = lpServiceStatusProcess[i].ServiceStatusProcess.dwProcessId;
+				}
+			}
+			delete[] lpServices;
+		}
+		CloseServiceHandle(scHandle);
+	}
+	if (PID == 0)
+		printf("[!]Get EventLog's PID error\n");
+
+	return PID;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("\nGet Eventlog Service PID and search evtx file's Handle.\n");
+		printf("Use NtQuerySystemInformation to query SystemHandleInformation.\n");
+		printf("Support:Win8 and later\n");
+		printf("Usage:\n");
+		printf("%s <evtx file name>\n", argv[0]);
+		return 0;
+	}
+	NTSTATUS status;
+	PSYSTEM_HANDLE_INFORMATION handleInfo;
+	ULONG handleInfoSize = 0x10000;
+	HANDLE processHandle;
+	ULONG i;
+	DWORD pid = getpid();
+	wchar_t buf1[100];
+	swprintf(buf1, 100, L"%hs", argv[1]);
+	_wcslwr_s(buf1, wcslen(buf1) + 1);
+
+	if (!EnableDebugPrivilege(TRUE))
+	{
+		printf("[!]AdjustTokenPrivileges Failed.<%d>\n", GetLastError());
+	}
+
+	_NtQuerySystemInformation NtQuerySystemInformation = (_NtQuerySystemInformation)GetProcAddress(GetModuleHandleA("NtDll.dll"), "NtQuerySystemInformation");
+	if (!NtQuerySystemInformation)
+	{
+		printf("[!]Could not find NtQuerySystemInformation entry point in NTDLL.DLL");
+		return 0;
+	}
+	_NtDuplicateObject NtDuplicateObject = (_NtDuplicateObject)GetProcAddress(GetModuleHandleA("NtDll.dll"), "NtDuplicateObject");
+	if (!NtDuplicateObject)
+	{
+		printf("[!]Could not find NtDuplicateObject entry point in NTDLL.DLL");
+		return 0;
+	}
+	_NtQueryObject NtQueryObject = (_NtQueryObject)GetProcAddress(GetModuleHandleA("NtDll.dll"), "NtQueryObject");
+	if (!NtQueryObject)
+	{
+		printf("[!]Could not find NtQueryObject entry point in NTDLL.DLL");
+		return 0;
+	}
+
+	if (!(processHandle = OpenProcess(PROCESS_DUP_HANDLE, FALSE, pid)))
+	{
+		printf("[!]Could not open PID %d!\n", pid);
+		return 0;
+	}
+	handleInfo = (PSYSTEM_HANDLE_INFORMATION)malloc(handleInfoSize);
+	while ((status = NtQuerySystemInformation(SystemHandleInformation, handleInfo, handleInfoSize, NULL)) == STATUS_INFO_LENGTH_MISMATCH)
+		handleInfo = (PSYSTEM_HANDLE_INFORMATION)realloc(handleInfo, handleInfoSize *= 2);
+	if (!NT_SUCCESS(status))
+	{
+		printf("[!]NtQuerySystemInformation failed!\n");
+		return 0;
+	}
+	for (i = 0; i < handleInfo->HandleCount; i++)
+	{
+		SYSTEM_HANDLE handle = handleInfo->Handles[i];
+		HANDLE dupHandle = NULL;
+		POBJECT_TYPE_INFORMATION objectTypeInfo;
+		PVOID objectNameInfo;
+		UNICODE_STRING objectName;
+		ULONG returnLength;
+
+		if (handle.ProcessId != pid)
+			continue;
+		if (!NT_SUCCESS(NtDuplicateObject(processHandle, (HANDLE)handle.Handle, GetCurrentProcess(), &dupHandle, 0, 0, 0)))
+		{
+			//			printf("[%#x] Error!\n", handle.Handle);
+			continue;
+		}
+		objectTypeInfo = (POBJECT_TYPE_INFORMATION)malloc(0x1000);
+		if (!NT_SUCCESS(NtQueryObject(dupHandle, ObjectTypeInformation, objectTypeInfo, 0x1000, NULL)))
+		{
+			//			printf("[%#x] Error!\n", handle.Handle);
+			CloseHandle(dupHandle);
+			continue;
+		}
+		objectNameInfo = malloc(0x1000);
+		if (!NT_SUCCESS(NtQueryObject(dupHandle, ObjectNameInformation, objectNameInfo, 0x1000, &returnLength)))
+		{
+			objectNameInfo = realloc(objectNameInfo, returnLength);
+			if (!NT_SUCCESS(NtQueryObject(dupHandle, ObjectNameInformation, objectNameInfo, returnLength, NULL)))
+			{
+				//				printf("[%#x] %.*S: (could not get name)\n", handle.Handle, objectTypeInfo->Name.Length / 2, objectTypeInfo->Name.Buffer);
+				free(objectTypeInfo);
+				free(objectNameInfo);
+				CloseHandle(dupHandle);
+				continue;
+			}
+		}
+		objectName = *(PUNICODE_STRING)objectNameInfo;
+		if (objectName.Length)
+		{
+			_wcslwr_s(objectName.Buffer, wcslen(objectName.Buffer) + 1);
+			if (wcsstr(objectName.Buffer, buf1) != 0)
+			{
+				printf("[+]HandleName:%.*S\n", objectName.Length / 2, objectName.Buffer);
+				printf("[+]Handle:%#x\n", handle.Handle);
+			}
+		}
+		else
+		{
+			//			printf("[%#x] %.*S: (unnamed)\n",handle.Handle,objectTypeInfo->Name.Length / 2,objectTypeInfo->Name.Buffer);
+		}
+		free(objectTypeInfo);
+		free(objectNameInfo);
+		CloseHandle(dupHandle);
+	}
+	free(handleInfo);
+	CloseHandle(processHandle);
+	return 0;
+}
